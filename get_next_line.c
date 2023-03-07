601a1c77b652ceaaf1cae96a97cd3bbad9bdd90d
RangTang-Aidan <30741060+eribu702@users.noreply.github.com>
2023-03-07 10:50:14 +1030

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroberts <aroberts@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 15:55:10 by aroberts          #+#    #+#             */
+/*   Updated: 2023/02/15 11:40:32 by aroberts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		size;
+	int		i;
+	char	*dup;
+
+	i = 0;
+	size = ft_strlen(str);
+	dup = malloc(sizeof(char) * (size + 1));
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*get_next_line(int fd)
+{
+	char	buffer;
+	char	line[7000000];
+	int		i;
+	int		b;
+
+	if (fd < 0)
+		return (NULL);
+	i = 0;
+	b = read(fd, &buffer, 1);
+	while (b > 0)
+	{
+		line[i++] = buffer;
+		if (buffer == '\n')
+			break ;
+		b = read(fd, &buffer, 1);
+	}
+	line[i] = '\0';
+	if (b <= 0 && i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
